7961c458efdd1c408729379198be44e14adc0b35
xissburg <xissburg@gmail.com>
2011-11-18 23:51:32 -0200

    Initial Commit

---
+++ VirtualAlvinn/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  VirtualAlvinn
+//
+//  Created by xiss burg on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
